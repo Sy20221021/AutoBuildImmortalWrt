10a0ea1e08b7c2ec9cfe28a23031997ad1ca4fca
Sy20221021 <116299354+Sy20221021@users.noreply.github.com>
2025-06-22 04:39:55 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -53,11 +53,11 @@ PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
 # ARP绑定
 PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
 # 端口转发
-# PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 # IP限速
-# PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
 # 流量控制
-# PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
 # 域名解析
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 # VPN服务
