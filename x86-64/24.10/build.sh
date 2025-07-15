6faae8ac65391e2d2e6458873458623a4189b2b5
Sy20221021 <116299354+Sy20221021@users.noreply.github.com>
2025-07-15 23:34:55 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -52,6 +52,33 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+# 定时唤醒
+PACKAGES="$PACKAGES luci-i18n-timewol-zh-cn"
+# 多线多拨
+PACKAGES="$PACKAGES luci-app-syncdial"
+# 多WAN负载均衡
+PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn"
+# 挂载CIFS网络共享
+PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
+# ARP绑定
+PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
+# 端口转发
+# PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+# IP限速
+# PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
+# 流量控制
+PACKAGES="$PACKAGES luci-i18n-sqm-zh-cn"
+# 域名解析
+PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+# VPN服务
+PACKAGES="$PACKAGES luci-app-openvpn-server"
+PACKAGES="$PACKAGES luci-i18n-openvpn-server-zh-cn"
+# VPN服务
+PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
+
+
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
@@ -70,13 +97,13 @@ PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 #PACKAGES="$PACKAGES luci-app-passwall2"
 #PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
 # VPN
-#PACKAGES="$PACKAGES luci-app-tailscale"
-#PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
+PACKAGES="$PACKAGES luci-app-tailscale"
+PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
 # 分区扩容 by sirpdboy 
-#PACKAGES="$PACKAGES luci-app-partexp"
-#PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn"
+PACKAGES="$PACKAGES luci-app-partexp"
+PACKAGES="$PACKAGES luci-i18n-partexp-zh-cn"
 # 酷猫主题 by sirpdboy 
-#PACKAGES="$PACKAGES luci-theme-kucat"
+PACKAGES="$PACKAGES luci-theme-kucat"
 # 网络测速 by sirpdboy 
 #PACKAGES="$PACKAGES luci-app-netspeedtest"
 #PACKAGES="$PACKAGES luci-i18n-netspeedtest-zh-cn"
